7b2a144e51a39a318b6dadd6acfb8bac4ff560d6
Drew <84882983+vdr3w@users.noreply.github.com>
2023-06-21 21:45:56 -0300

    att pag cadastro

--- semana2/aula3/clone instagram/scripts/cadastro.js
+++ semana2/aula3/clone instagram/scripts/cadastro.js
@@ -1,14 +1,18 @@
 function fazerCadastro() {
   var nome = document.getElementById("campo-nome-completo").value;
+  var email = document.getElementById("campo-email").value;
+  var telefone = document.getElementById("campo-telefone").value;
+  var senha = document.getElementById("campo-senha").value;
 
   if (nome === "") {
     document.getElementById("campo-nome-completo").classList.add("input-error");
     document.getElementById("campo-nome-completo").focus();
     document.getElementById("error-nome").hidden = false;
     document.getElementById("error-nome").innerText = "O Nome é Obrigatório";
+  } else if (senha.length < 9) {
+    document.getElementById("campo-senha").classList.add("input-error");
+    document.getElementById("error-senha").hidden = false;
+    document.getElementById("error-senha").innerText =
+      "A senha precisa ter no minimo 9 caracteres";
   }
-
-  var email = document.getElementById("campo-email").value;
-  var telefone = document.getElementById("campo-telefone").value;
-  var senha = document.getElementById("campo-senha").value;
 }
