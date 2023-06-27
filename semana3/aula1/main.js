28991bdb7d85581db05fcb4c187a49c1e2909629
Drew <84882983+vdr3w@users.noreply.github.com>
2023-06-26 21:56:52 -0300

    Aula 1 - Funções & Arrow Functions

--- semana3/aula1/main.js
+++ semana3/aula1/main.js
@@ -1,12 +1,13 @@
 console.log("oi");
 
-// exemplo
+// < Exemplo Função Comum
+
 // f(x, y) = x * 2 - y
 // f(2, 3) = 2 * 2 - 3
 // f(2, 3) = 4 - 3
 // f(2, 3) = 1
 
-// funcao em cod
+// função em js
 function f(x, y) {
   const resultado = x * 2 - y;
   return resultado;
@@ -14,3 +15,43 @@ function f(x, y) {
 
 const retorno = f(2, 3);
 console.log(retorno);
+// />
+
+// Exemplo Função Efemera (Temporaria/Anonima)
+(function () {
+  console.log("Executou a funcao!");
+})();
+// />
+
+// Exemplo Arrow Function
+
+// comparação com funcao normal dentro de variavel
+const funcaoComum = function () {
+  console.log("Função Comum");
+};
+
+funcaoComum();
+
+// Exemplo Equivalencias 1
+// Arrow Function
+const funcaoArrow = (a) => {
+  return "Funcao arrow. " + a;
+};
+
+// const funcaoArrow = (a) => "Função Arrow" + a;
+console.log(funcaoArrow("Drew"));
+// />
+
+// Exemplo Equivalencias 2
+function executaEssa(funcaoExterna) {
+  const retorno = funcaoExterna(3);
+  console.log(retorno);
+}
+
+// Funcao comum passa por parametro
+executaEssa(function (a) {
+  return a * 2;
+});
+// Arrow Function passada por parametro
+executaEssa((a) => a * 2);
+// />
