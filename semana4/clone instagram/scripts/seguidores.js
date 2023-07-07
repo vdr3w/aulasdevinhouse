4699c211408b9f1c59ae3c07ad3848c423ed0a5f
Drew <84882983+vdr3w@users.noreply.github.com>
2023-07-06 21:20:37 -0300

    Clone instagram

--- semana4/clone instagram/scripts/seguidores.js
+++ semana4/clone instagram/scripts/seguidores.js
@@ -1,14 +1,39 @@
 import { seguidores } from "../constantes/seguidores.js";
 
+// Criando uma cópia dos seguidores com um apelido adicionado
+const seguidoresFormatados = seguidores.map((seguidor) => {
+  return {
+    ...seguidor,
+    // Criando o apelido: removendo acentos, convertendo para letras minúsculas e substituindo espaços por underscores
+    apelido:
+      "#" +
+      seguidor.nome
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .toLowerCase()
+        .replace(" ", "_"),
+  };
+});
+
+// USAR OPERADORES LOGICOS PARA PODER RECEBER QUALQUER TIPO DE INPUT <<< TODO TODO
 // Definindo um event listener para o formulário de pesquisa
 document.getElementById("form-pesquisa").addEventListener("submit", (event) => {
   event.preventDefault(); // Prevenindo o comportamento padrão do formulário (recarregar a página)
 
   const valorPesquisa = document.getElementById("input-pesquisa").value; // Pegando o valor do input de pesquisa
 
-  // Filtrando os seguidores pelo nome com base no valor de pesquisa
-  const seguidoresFiltrados = seguidores.filter(
-    (seguidor) => seguidor.nome === valorPesquisa
+  // Filtrando os seguidores pelo nome com base no valor de pesquisa, deixando-os caixa baixa, e o resultado tambem
+  const seguidoresFiltrados = seguidoresFormatados.filter((seguidor) =>
+    seguidor.nome
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .toLowerCase()
+      .includes(
+        valorPesquisa
+          .normalize("NFD")
+          .replace(/[\u0300-\u036f]/g, "")
+          .toLowerCase()
+      )
   );
 
   document.getElementById("corpo-tabela").innerHTML = ""; // Limpando o corpo da tabela antes de adicionar novas linhas
@@ -22,7 +47,7 @@ document.getElementById("form-pesquisa").addEventListener("submit", (event) => {
     tr.appendChild(tdFoto);
 
     const tdNome = document.createElement("td");
-    tdNome.innerHTML = `${seguidor.nome}`;
+    tdNome.innerHTML = `${seguidor.nome} (${seguidor.apelido})`;
     tr.appendChild(tdNome);
 
     const tdSeguidores = document.createElement("td");
@@ -39,21 +64,6 @@ document.getElementById("form-pesquisa").addEventListener("submit", (event) => {
 
 // Função que cria linhas na tabela para cada seguidor
 function criarLinhasTabela() {
-  // Criando uma cópia dos seguidores com um apelido adicionado
-  const seguidoresFormatados = seguidores.map((seguidor) => {
-    return {
-      ...seguidor,
-      // Criando o apelido: removendo acentos, convertendo para letras minúsculas e substituindo espaços por underscores
-      apelido:
-        "#" +
-        seguidor.nome
-          .normalize("NFD")
-          .replace(/[\u0300-\u036f]/g, "")
-          .toLowerCase()
-          .replace(" ", "_"),
-    };
-  });
-
   // Iterando sobre os seguidores formatados e criando uma linha de tabela para cada um
   seguidoresFormatados.map((seguidor) => {
     const tr = document.createElement("tr");
@@ -78,5 +88,7 @@ function criarLinhasTabela() {
     document.getElementById("corpo-tabela").appendChild(tr);
   });
 }
+
+function gerarLinhasTabela() {}
 
 window.onload = criarLinhasTabela;
