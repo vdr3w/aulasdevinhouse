a7c3db9b6b0b26adf3f09d9541adae91dc51761d
Drew <84882983+vdr3w@users.noreply.github.com>
2023-07-10 20:57:50 -0300

    Aula 1 Semana 5

---
+++ semana5/aula1/main.js
@@ -0,0 +1,13 @@
+const lista = document.getElementById("lista");
+
+console.log({ lista });
+
+// Recebe HTML Collection (similar a array)
+const itens = document.getElementsByTagName("li");
+
+// Converte um HTML Collection em Array
+const itensArray = [...itens];
+
+const itensLista1 = document.querySelectorAll("#lista1 li");
+
+console.log({ itensLista1 });
