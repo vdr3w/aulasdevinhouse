5832837387b7e38e79fbfaf04a661483c8edeb11
Drew <84882983+vdr3w@users.noreply.github.com>
2023-08-03 16:47:14 -0300

    Semana 7 - Dia 3

--- semana7/vue-devinhouse/src/App.vue
+++ semana7/vue-devinhouse/src/App.vue
@@ -1,11 +1,36 @@
 <template>
   <div>
-    <h1>teste</h1>
+    <h1>Calculadora IMC.vue</h1>
+    {{ peso }}
+    <input type="number" placeholder="Peso" v-model="peso" />
+
+    {{ altura }}
+    <input type="number" placeholder="Altura" v-model="altura" />
+    <button @click="calcularImc()">Calcular</button>
+    <span>{{ resultado }}</span>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      peso: 0,
+      altura: 0,
+      resultado: "",
+    };
+  },
+  methods: {
+    calcularImc() {
+      if (this.peso == 0 || this.altura == 0) {
+        alert("Dados precisam ser diferentes de 0");
+        return;
+      }
+
+      this.resultado = (this.peso / this.altura ** 2).toFixed(2);
+    },
+  },
+};
 </script>
 
 <style></style>
