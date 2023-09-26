70b90b484f2685b6b3e5c27a7e3b32e234ebd8e4
Drew <84882983+vdr3w@users.noreply.github.com>
2023-09-26 14:51:47 -0300

    clone insta

--- semana8/clone-instagram/src/pages/Posts/List/ListPost.vue
+++ semana8/clone-instagram/src/pages/Posts/List/ListPost.vue
@@ -1,11 +1,25 @@
 <template>
   <h1>Listagem de posts</h1>
 
-  <ul>
-    <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
-  </ul>
-
-  <button @click="loadPosts">Carregar posts</button>
+  <v-table>
+    <thead>
+      <tr>
+        <th class="text-left">Código</th>
+        <th class="text-left">Título</th>
+        <th class="text-left">Descrição</th>
+        <th class="text-left">Ações</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="post in posts" :key="post.id">
+        <td>{{ post.id }}</td>
+        <td><img :src="post.url" width="50" /></td>
+        <td>{{ post.title }}</td>
+        <td>{{ post.description }}</td>
+        <v-btn @click="() => deletarPost(post.id)">Deletar</v-btn>
+      </tr>
+    </tbody>
+  </v-table>
 </template>
 
 <script>
@@ -16,6 +30,9 @@ export default {
     return {
       posts: [],
     };
+  },
+  mounted() {
+    this.loadPosts();
   },
   methods: {
     loadPosts() {
@@ -30,10 +47,28 @@ export default {
       })
         .then((response) => {
           this.posts = response.data.posts;
-          alert("deu certo");
+          console.log("deu certo");
         })
         .catch(() => {
-          alert("deu ruim");
+          console.log("deu ruim");
+        });
+    },
+    deletarPost(id) {
+      console.log("deletarPost");
+      const token = localStorage.getItem("instagram_token");
+
+      axios({
+        url: "http://localhost:3000/api/posts/" + id,
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearen ${token}`,
+        },
+      })
+        .then(() => {
+          this.loadPosts();
+        })
+        .catch(() => {
+          console.log("erro ao deletar");
         });
     },
   },
